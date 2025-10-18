1d6802f5568d378523a42e2b961488f2d1516e44
Xingyu Lu <estrellalxy@gmail.com>
2025-10-18 15:33:21 +0000

    part1 d -half

--- material/part1/src/AppState.jsx
+++ material/part1/src/AppState.jsx
@@ -8,11 +8,21 @@ const App = () => {
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-    setTotal(left + right)
+    console.log('left before', left)
+    setLeft(left + 1) 
+    console.log('left after', left)  // the same as before
+    setTotal(left + right) 
   }
+  /* Reason: 
+  React maintains a event listener at root of DOM. 
+  If there are event happens in DOM(e.g. click), it will bubbles out to the root listener 
+  React finds thhe fibre where the event happens, if this fiber have event handler to execute, 
+  Synthetic Event System of React will integrate a event object, 
+  then React call batchedEventUpdates() on this event, which will ensure that all setState in the event handler will not re-render the component immediately. 
+  Instead, after the function is finished, it will only render once to update all the states cahnged in this function
+  */
 
-    const handleRightClick = () => {
+  const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     setRight(right + 1)
     setTotal(left + right)
--- material/part1/src/main.jsx
+++ material/part1/src/main.jsx
@@ -1,8 +1,10 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './App' // euqals to: import App from './App.jsx'; building tools will automatically try to find: js, jsx, ts, tsx..
+// import App from './App' // euqals to: import App from './App.jsx'; building tools will automatically try to find: js, jsx, ts, tsx..
 // import App from './AppSec'  
 // import App from './AppClick'
 // import App from './AppReuse'
+import App from './ConditionalRe'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
