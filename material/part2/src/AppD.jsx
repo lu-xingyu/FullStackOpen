43f85f62e125e13849a825a4affed93a3739a53f
Xingyu Lu <estrellalxy@gmail.com>
2025-10-20 13:54:29 +0000

    part2 d half finished

---
+++ material/part2/src/AppD.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+import noteService from './services/notes'
+
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+
+  useEffect(() => {
+    console.log('effect')
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)  // event.target is the DOM element that intrigues the event, here is the input
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+        content: newNote,
+        important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+
+  }
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+        .update(id, changedNote)
+        .then(returnedNote => {
+            setNotes(notes.map(note => note.id === id ? returnedNote : note)) //if node.id=id, return data from server; else return the original data in notes
+        })
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)} />
+        )}
+      </ul>
+
+      <form onSubmit={addNote}>          {/* React automatically creates en event object and call addNote(event) */}
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+
+export default App 
--- material/part2/src/main.jsx
+++ material/part2/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './AppC'
+import App from './AppD'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
