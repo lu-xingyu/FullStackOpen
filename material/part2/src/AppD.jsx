e4175ed696c2902022cf9af12f74a13e8d2c61f0
Xingyu Lu <estrellalxy@gmail.com>
2025-10-21 12:50:20 +0000

    at library, part 2 d half

--- material/part2/src/AppD.jsx
+++ material/part2/src/AppD.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
 
   useEffect(() => {
@@ -56,9 +59,12 @@ const App = () => {
             setNotes(notes.map(note => note.id === id ? returnedNote : note)) //if node.id=id, return data from server; else return the original data in notes
         })
         .catch(error => {
-          alert(
-            `the note '${note.content}' was already deleted from server`
+          setErrorMessage(
+            `Note '${note.content}' was already removed from server`
           )
+          setTimeout(() => {
+          setErrorMessage(null)
+          }, 5000)
           setNotes(notes.filter (n => n.id !== id))
         })
   }
@@ -66,6 +72,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -83,7 +90,8 @@ const App = () => {
           onChange={handleNoteChange}
         />
         <button type="submit">save</button>
-      </form>   
+      </form>  
+      <Footer /> 
     </div>
   )
 }
--- material/part2/src/services/notes.js
+++ material/part2/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'https://didactic-halibut-q7pxpr6qv64g346j7-3001.app.github.dev/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
