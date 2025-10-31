198f34b9a790a48b862334ad53ff4a69d6d57a1f
Esther Lu <estrellalxy@gmail.com>
2025-10-31 11:45:02 +0100

    ready for 3.12 ex

--- material/part3/mongo.js
+++ material/part3/mongo.js
@@ -7,25 +7,39 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://estrellalxy_db_user:${password}@cluster0.a5qfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://estrellalxy_db_user:${password}@cluster0.addi0ax.mongodb.net/noteApp?appName=Cluster0`
 
-mongoose.set('strictQuery',false)
-
-mongoose.connect(url)
+mongoose.connect(url)   // connect to database, return a promise
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+})   // define schema of collection
 
 const Note = mongoose.model('Note', noteSchema)
+// Create a inner model in mongoDB named Note, and create a collection named "notes" according to noteSchema, 
+// and return the inner model as a class, which can be used to operate the collection
 
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+/*
 const note = new Note({
-  content: 'HTML is easy',
+  content: 'Mongogoose makes things easy',
   important: true,
 })
+
+// create object / instance of Note class
 
 note.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
 })
+*/
+// save() note object in to collection, return a promise
+// .then() call th Fn after the note is successfully saved
